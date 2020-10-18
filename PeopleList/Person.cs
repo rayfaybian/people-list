31320d366118e74f5035bb962c7d3481157a5b67
RayFaybian <fabian.suppan@hotmail.com>
2020-10-18 21:33:59 +0200

    first commit

---
+++ PeopleList/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeineErsteApp
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string city;
+        public string Job;
+
+        override public String ToString()
+        {
+            return name + ";" + age + ";" + city + ";" + Job;
+        }
+    }
+}
---
+++ PeopleList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MeineErsteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFileContent = System.IO.File.ReadAllLines("C:\\Coding\\C#\\PeopleList\\Files\\people.txt");
+            string[] strArray;
+
+            int counter = 0;
+            foreach (string element in myFileContent)
+            {
+                strArray = element.Split(";");
+
+                Person person = new Person();
+                person.name = strArray[0];
+                person.age = int.Parse(strArray[1]);
+                person.city = strArray[2];
+
+                if (person.name.StartsWith("H"))
+                {
+                    person.Job = "Coder";
+                }
+                else
+                {
+                    person.Job = "Officer";
+                }
+
+                Console.WriteLine("Name: " + person.name + "\nAge: " + person.age + "\nCity: " + person.city + "\nJob: " + person.Job + "\n");
+
+                myFileContent[counter] = person.ToString();
+                counter++;
+
+            }
+
+            System.IO.File.WriteAllLines("C:\\Coding\\C#\\PeopleList\\Files\\peopleNEW.txt", myFileContent);
+        }
+    }
+}
